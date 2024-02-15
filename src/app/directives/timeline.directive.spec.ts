ecd45dd7fc40c9fcbb244b6170a98ab340e8ea87
saiprasad. <saiprasad@rajutech.com>
2024-02-15 11:37:32 +0530

    Auto-generated commit

--- src/app/directives/timeline.directive.spec.ts
+++
@@ -1,9 +0,0 @@
-import { ElementRef, Renderer2 } from '@angular/core';
-import { TimelineDirective } from './timeline.directive';
-
-describe('TimelineDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TimelineDirective(<ElementRef>{}, <Renderer2>{});
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/directives/toggle-nav-menu.directive.spec.ts
+++
@@ -1,9 +0,0 @@
-import { ElementRef } from '@angular/core';
-import { ToggleNavMenuDirective } from './toggle-nav-menu.directive';
-
-describe('ToggleNavMenuDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ToggleNavMenuDirective();
-    expect(directive).toBeTruthy();
-  });
-});
